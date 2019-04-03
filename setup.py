3530fcaa7bfbef44fe5442b5cca4990dbcb74df7
Lethys <yandawl@gmail.com>
2019-04-03 19:02:19 +0100

    Execution time decorator should be async

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="xivapi.py",
-    version="0.1.0",
+    version="0.1.1",
     author="Lethys",
     author_email="seraymericbot@gmail.com",
     license="MIT",
--- xivapi/decorators.py
+++ xivapi/decorators.py
@@ -8,10 +8,9 @@ __log__ = logging.getLogger(__name__)
 def timed(func):
     """This decorator prints the execution time for the decorated function."""
     @wraps(func)
-    def wrapper(*args, **kwargs):
+    async def wrapper(*args, **kwargs):
         start = time()
-        result = func(*args, **kwargs)
-        end = time()
-        __log__.info("{} ran in {}s".format(func.__name__, round(end - start, 2)))
+        result = await func(*args, **kwargs)
+        __log__.info("{} executed in {}s".format(func.__name__, round(time() - start, 2)))
         return result
     return wrapper
