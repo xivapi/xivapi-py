7958d57be408d7e0e724d915bed23c52fc5f012a
Lethys <yandawl@gmail.com>
2020-02-05 20:56:57 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(
-    name="xivapi.py",
+    name="xivapi-py",
     version="0.1.1",
     author="Lethys",
     author_email="seraymericbot@gmail.com",
@@ -9,7 +9,7 @@ setuptools.setup(
     description="An asynchronous Python client for XIVAPI",
     keywords='ffxiv xivapi',
     long_description_content_type="text/markdown",
-    url="https://github.com/Yandawl/xivapi.py",
+    url="https://github.com/Yandawl/xivapi-py",
     packages=setuptools.find_packages(),
     install_requires=['asyncio', 'aiohttp'],
     python_requires='>=3.6.0',
