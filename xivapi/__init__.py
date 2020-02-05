341b1b604df90b65932dd28b543a6bb27445b405
Lethys <yandawl@gmail.com>
2020-02-05 22:38:44 +0000

    Update __init__.py

--- xivapi/__init__.py
+++ xivapi/__init__.py
@@ -2,7 +2,7 @@ __title__ = 'xivapi-py'
 __author__ = 'Lethys'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2019 (c) Lethys'
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 from .client import Client
 from .exceptions import *
