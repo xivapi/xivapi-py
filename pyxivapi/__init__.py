1e1a1ffcfe047503dc72270d5191849ab2b66711
Lethys <yandawl@gmail.com>
2020-08-08 20:26:29 +0100

    Update version to 0.2.1

--- pyxivapi/__init__.py
+++ pyxivapi/__init__.py
@@ -2,7 +2,7 @@ __title__ = 'pyxivapi'
 __author__ = 'Lethys'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2019 (c) Lethys'
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 from .client import XIVAPIClient
 from .exceptions import *
